7c93d15825ca1912905d07096627b47b100dd351
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-10 20:13:12 +0930

    add index.js with one to many realtions with user, post and comment

---
+++ models/index.js
@@ -0,0 +1,37 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// user and post realtionship
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// user and comment relationship
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// post and comment relationship
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
