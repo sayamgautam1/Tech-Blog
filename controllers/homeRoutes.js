5263ab756c7dad520d091a87b3a56293649d05f5
Sayam Gautam <sayamgautam1@gmail.com>
2022-06-10 21:15:49 +0930

    add home routes to get all the post when the homepage route is called:

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    // get all the posted blogs till date
+
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // serialize data so the template can read it
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // pass serialized data and session flag into handle bar template to render
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
